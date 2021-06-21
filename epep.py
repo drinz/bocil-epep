bd645c8946778e6e3f1c02f9bd9617147a1a36e5
drinz <86057200+drinz@users.noreply.github.com>
2021-06-21 17:37:45 +0700

    Create epep.py

---
+++ epep.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+
+print("--> tools hack by iki <--")
+print("|----------|")
+print("|hello|")
+print("|__________|")
+ip = str(input(" username ff:"))
+port = str(input(" id:"))
+
+
+print("menghack akun ff yang bernama %s dengan id %s"%(ip,port))
+hello = str(input("apakah anda yakin untuk melihat password akun?"))
+print("password akun %s adalah prank aowkwkkw"%(ip))
